2666e2ed8de4ae3be53a4b80a12ec779ec90d117
EdmirSoares <dimisoares10@gmail.com>
2025-10-06 23:09:45 -0300

    Refactor code structure for improved readability and maintainability

--- metro.config.js
+++ metro.config.js
@@ -1,12 +1,21 @@
 // Learn more https://docs.expo.io/guides/customizing-metro
 const { getDefaultConfig } = require('expo/metro-config');
-
-  const { withNativeWind } = require("nativewind/metro");
-
+const { withNativeWind } = require("nativewind/metro");
 
 /** @type {import('expo/metro-config').MetroConfig} */
 // eslint-disable-next-line no-undef
 const config = getDefaultConfig(__dirname);
 
+// Configure SVG transformer
+config.transformer = {
+  ...config.transformer,
+  babelTransformerPath: require.resolve('react-native-svg-transformer'),
+};
 
-  module.exports = withNativeWind(config, { input: "./global.css" });
+config.resolver = {
+  ...config.resolver,
+  assetExts: config.resolver.assetExts.filter((ext) => ext !== 'svg'),
+  sourceExts: [...config.resolver.sourceExts, 'svg'],
+};
+
+module.exports = withNativeWind(config, { input: "./global.css" });
